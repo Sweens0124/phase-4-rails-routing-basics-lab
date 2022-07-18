e28d824fa05bbe07159940b3f47f7902a1074824
Lucas Sweeney <lsweeney0124@gmail.com>
2022-07-18 11:19:36 -0400

    commit tree 3f4d09218097fecbd6a85c3d7e05dee52333da6b

---
+++ app/controllers/student_controller.rb
@@ -0,0 +1,11 @@
+class StudentController < ApplicationController
+  def index
+    students = Student.all 
+    render json: students 
+  end
+
+  def grades 
+    students = Student.order(grade: :desc)
+    render json: students
+  end
+end
